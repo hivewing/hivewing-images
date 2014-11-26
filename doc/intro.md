fa485390afb5f3660120a2ea01a96bba5fad3a6e
gitolite tester <tester@example.com>
2014-11-26 13:09:37 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hivewing-images
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/hivewing_images/core_test.clj
@@ -0,0 +1,7 @@
+(ns hivewing-images.core-test
+  (:require [clojure.test :refer :all]
+            [hivewing-images.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
